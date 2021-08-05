a54be2d7118f81a79568a7c2901fb760255d0f9e
TN Botz <77948100+rubandurai27@users.noreply.github.com>
2021-08-05 12:19:45 +0530

    Update progress_bar.py

--- renamer/tools/progress_bar.py
+++ renamer/tools/progress_bar.py
@@ -19,19 +19,19 @@ async def progress_bar(current, total, status_msg, start, msg):
         time_to_complete = round(((total - current) / speed)) * 1000
         time_to_complete = TimeFormatter(time_to_complete)
         progressbar = "[{0}{1}]".format(\
-            ''.join([f"{BLACK_MEDIUM_SMALL_SQUARE}" for i in range(math.floor(percentage / 10))]),
-            ''.join([f"{WHITE_MEDIUM_SMALL_SQUARE}" for i in range(10 - math.floor(percentage / 10))])
+            ''.join([f"{●}" for i in range(math.floor(percentage / 5))]),
+            ''.join([f"{○}" for i in range(20 - math.floor(percentage / 5))])
             )
         current_message = f"""**{status_msg}** {round(percentage, 2)}%
 {progressbar}
 
-{HOLLOW_RED_CIRCLE} **Speed**: {humanbytes(speed)}/s
+ **Speed**🚀: {humanbytes(speed)}/s
 
-{HOLLOW_RED_CIRCLE} **Done**: {humanbytes(current)}
+ **Done**✅: {humanbytes(current)}
 
-{HOLLOW_RED_CIRCLE} **Size**: {humanbytes(total)}
+**Size**🌀: {humanbytes(total)}
 
-{HOLLOW_RED_CIRCLE} **Time Left**: {time_to_complete}"""
+**Time Left🕰**: {time_to_complete}"""
         try:
             await msg.edit(text=current_message)
         except:
